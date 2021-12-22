af93eb5c268b26c3e9ea9f75b6e063acbc51d7dc
Falah Hassan <FALAH.cs79@student.uomosul.edu.iq>
2021-12-23 01:13:24 +0300

    refactor code

--- readable/lib/src/extensions/string/string.dart
+++ readable/lib/src/extensions/string/string.dart
@@ -1,12 +1,5 @@
 ///
 extension StringReadableX on String {
   ///convert  String to List of Characters
-  List<String> get toChars {
-    // final List<String> chars = [];
-    // for (int i = 0; i < length; i++) {
-    //   chars.add(this[i]);
-    // }
-    // return chars;
-    return split('');
-  }
+  List<String> toChars() => split('');
 }
