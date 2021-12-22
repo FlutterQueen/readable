abcef20388e2775d23a64d21524e9a3cd24accb3
Hazem_ELgohary <hazemelgohary471@gmail.com>
2021-12-22 15:49:46 +0200

    update tests

--- readable/test/extensions/list_test.dart
+++ readable/test/extensions/list_test.dart
@@ -3,10 +3,8 @@ import 'package:test/expect.dart';
 import 'package:test/scaffolding.dart';
 
 void main() {
-  test('it return a random element from the list', (){
+  test('it return a random element from the list', () {
     const items = [1, 2, 3, 'H', 5];
-    final randomElement = items.random;
-
-    expect(items.contains(randomElement), isTrue);
+    expect(items.contains(items.random), isTrue);
   });
 }
