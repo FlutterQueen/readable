4b1575643d715049a8516d4ef91f52ea97a666b3
Hazem_ELgohary <hazemelgohary471@gmail.com>
2021-12-23 00:42:25 +0200

    add (only) method

--- readable/lib/src/extensions/map/map.dart
+++ readable/lib/src/extensions/map/map.dart
@@ -1,11 +1,20 @@
-// extension MapExt<K, V> on Map<K, V> {
-//   Map<K, V> whereNull() {
-//     final map = <K, V>{};
-//     for (final entry in entries) {
-//       if (entry.value != null) {
-//         map[entry.key] = entry.value;
-//       }
-//     }
-//     return map;
-//   }
-// }
+///
+extension MapExt<K, V> on Map<K, V> {
+  // Map<K, V> whereNull() {
+  //   final map = <K, V>{};
+  //   for (final entry in entries) {
+  //     if (entry.value != null) {
+  //       map[entry.key] = entry.value;
+  //     }
+  //   }
+  //   return map;
+  // }
+  /// return only key i need it
+  Map<K, V?> only(List<K> keys) {
+    final holder = <K, V?>{};
+    for (final key in keys) {
+      holder[key] = this[key];
+    }
+    return holder;
+  }
+}
