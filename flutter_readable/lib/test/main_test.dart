22b939a349c9a36e430e4eb6b6593fb8b793b1d0
Ahmed Masoud <maxzod66@gmail.com>
2021-12-23 01:19:33 +0200

    fix tests

---
+++ flutter_readable/lib/test/main_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_readable/flutter_readable.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test(
+    '',
+    () => expect(foo(50), 100),
+  );
+}
