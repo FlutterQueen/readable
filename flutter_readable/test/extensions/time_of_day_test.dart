87df6414ff3f9eb43da7ec573c131a21f7e0c1c4
Falah Hassan <FALAH.cs79@student.uomosul.edu.iq>
2021-12-25 15:44:45 +0300

    TimeOfDay test

---
+++ flutter_readable/test/extensions/time_of_day_test.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_readable/src/extensions/time_of_day.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test(
+    'TimeOfDay',
+    () {
+      const after = TimeOfDay(
+        hour: 9,
+        minute: 15,
+      );
+      expect(const TimeOfDay(hour: 13, minute: 10).in12Format, "1:10");
+      expect(const TimeOfDay(hour: 15, minute: 15).in12Format, "3:15");
+      expect(const TimeOfDay(hour: 13, minute: 10).isAm, isFalse);
+      expect(const TimeOfDay(hour: 8, minute: 15).isAm, isTrue);
+      expect(const TimeOfDay(hour: 13, minute: 10).isPm, isTrue);
+      expect(const TimeOfDay(hour: 8, minute: 15).isPm, isFalse);
+      expect(
+        const TimeOfDay(hour: 8, minute: 15).isAfter(after),
+        isFalse,
+      );
+      expect(
+        const TimeOfDay(hour: 10, minute: 15).isAfter(after),
+        isTrue,
+      );
+      expect(
+        const TimeOfDay(hour: 8, minute: 15).isBefore(after),
+        isTrue,
+      );
+      expect(
+        const TimeOfDay(hour: 8, minute: 15)
+            .isBetween(after.copyWith(hour: 7), after),
+        isTrue,
+      );
+      expect(
+        const TimeOfDay(hour: 8, minute: 15).copyWith(hour: 9),
+        after,
+      );
+    },
+  );
+}
